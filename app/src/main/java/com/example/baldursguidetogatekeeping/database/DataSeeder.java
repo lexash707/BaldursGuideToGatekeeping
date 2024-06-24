02d49355364d938cea45e1200f4a4a6b73928906
lexash707 <lexashart@gmail.com>
2024-06-24 05:18:49 +0200

    -

--- app/src/main/java/com/example/baldursguidetogatekeeping/database/DataSeeder.java
+++ app/src/main/java/com/example/baldursguidetogatekeeping/database/DataSeeder.java
@@ -17,7 +17,6 @@ public class DataSeeder {
             @Override
             protected Void doInBackground(Void... voids) {
                 PlayableCharacter astarion = new PlayableCharacter("Astarion", "Rogue", "High Elf", "Astarion has the demeanour of a charming and confident nobleman with a biting wit. He enjoys making sarcastic remarks and japes at other people's expense, all the while acting smug and haughty. His decades of being a vampire have left him with a lack of moral scruples compared to the more upstanding members of the party, making him come off as ruthless at best or outright sadistic at worst. While he does attempt to suck the player’s blood non-consensually during one of the first long rests after recruiting him, he does respect the player’s decision regardless of whether it is to allow him to drink or forbid him from drinking their blood.", R.mipmap.astarion);
-
                 PlayableCharacter karlach = new PlayableCharacter("Karlach", "Barbarian", "Tiefling", "Karlach is an extremely passionate individual who strives to live life to the fullest. She tends to speak excitedly regarding any of life's simple pleasures, whether that be food, drinks, friends, or intimacy. Her demeanor is generally joyous and very forward, having no fear of loudly proclaiming her love of being alive and how much she cares for those close to her. Her positive disposition and infectious energy seem to allow her to make friends easily, even if her honesty can cause her to come off as somewhat naive at times.[note 2] Karlach is also very protective of her friends and companions, taking a genuine interest in them, especially with Wyll should he have been transformed into a devil after defending her", R.mipmap.karlach);
                 PlayableCharacter gale = new PlayableCharacter("Gale", "Wizard", "Human", "You're a wizard Gale", R.mipmap.gale);
 
@@ -33,7 +32,7 @@ public class DataSeeder {
         }.execute();
     }
 
-        protected static RoomDatabase.Callback databaseCallback = new RoomDatabase.Callback() {
+    protected static RoomDatabase.Callback databaseCallback = new RoomDatabase.Callback() {
         @Override
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
             super.onCreate(db);
